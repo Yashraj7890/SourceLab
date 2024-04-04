8563da97d4d953da386d2dd85bd6b8d31f602e9f
Yash <yashrajsingh7890@gmail.com>
2024-04-05 04:54:24 +0530

    sameSite

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,15 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: true, 
+        sameSite: 'none'
+    }
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
